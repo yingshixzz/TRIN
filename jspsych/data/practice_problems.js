0f6aec89a89c63f09bbb2bf7ba3c62956bac74d7
yingshixzz <59970930+yingshixzz@users.noreply.github.com>
2023-09-14 13:27:36 -0400

    Add files via upload

---
+++ jspsych/data/practice_problems.js
@@ -0,0 +1,173 @@
+/**
+ * practice_problems.js
+ * Kyoung whan Choe (https://github.com/kywch/)
+**/
+
+/* ************************************ */
+/* Variable definitions */
+/* ************************************ */
+// the problems are in the order of appearance
+var practice_problems = [];
+// the problems will be shuffled
+var practice_choice = [];
+
+
+/* ************************************ */
+/* practice_problems */
+/* ************************************ */
+practice_problems.push({
+    "probid": "1",
+    "type": "s1",
+    "make": "Ford",
+    "body": "Truck",
+    "age": "12",
+    "weight": "Class 8: 33,001 lb and above (14,969 kg and above)",
+    "region": "Micropolitan",
+    "odometer": "15763",
+    "mileage": "28254",
+    "location": "Left Front",
+    "thickness": "18/32inch",
+    "answer": "2",
+    "orgword": "QUIZ"
+});
+practice_problems.push({
+    "probid": "2",
+    "type": "s2",
+    "make": "make2",
+    "age": "12",
+    "weight": "Class 7: 26,001 - 33,000 lb (11,794 - 14,969 kg)",
+    "region": "Micropolitan",
+    "odometer": "15763",
+    "mileage": "28254",
+    "location": "Left Front",
+    "thickness": "11/32inch",
+    "answer": "1",
+    "orgword": "NURSERY"
+});
+practice_problems.push({
+    "probid": "3",
+    "type": "d1",
+    "make": "make3",
+    "age": "12",
+    "weight": "Class 8: 33,001 lb and above (14,969 kg and above)",
+    "region": "Micropolitan",
+    "odometer": "15763",
+    "mileage": "28254",
+    "location": "Left Front",
+    "thickness": "12/32inch",
+    "answer": "1",
+    "orgword": "PLAN"
+});
+practice_problems.push({
+    "probid": "4",
+    "type": "d2",
+    "make": "make4",
+    "age": "12",
+    "weight": "Class 7: 26,001 - 33,000 lb (11,794 - 14,969 kg)",
+    "region": "Micropolitan",
+    "odometer": "15763",
+    "mileage": "28254",
+    "location": "Left Front",
+    "thickness": "4/32inch",
+    "answer": "3",
+    "orgword": "ARTWORK"
+});
+practice_problems.push({
+    "probid": "5",
+    "type": "s1",
+    "make": "International",
+    "age": "12",
+    "weight": "Class 8: 33,001 lb and above (14,969 kg and above)",
+    "region": "Micropolitan",
+    "odometer": "15763",
+    "mileage": "28254",
+    "location": "Left Front",
+    "thickness": "7/32inch",
+    "answer": "1",
+    "orgword": "BEAUTY"
+});
+
+practice_problems.push({
+    "probid": "6",
+    "type": "d2",
+    "make": "w1",
+    "age": "12",
+    "weight": "Class 8: 33,001 lb and above (14,969 kg and above)",
+    "region": "Medium metro",
+    "odometer": "15763",
+    "mileage": "28254",
+    "location": "Left Front",
+    "thickness": "3/32inch",
+    "answer": "1",
+    "orgword": "USAGE"
+});
+practice_problems.push({
+    "probid": "7",
+    "type": "s1",
+    "make": "w1",
+    "age": "12",
+    "weight": "Class 7: 26,001 - 33,000 lb (11,794 - 14,969 kg)",
+    "region": "Micropolitan",
+    "odometer": "15763",
+    "mileage": "28254",
+    "location": "Left Front",
+    "thickness": "9/32inch",
+    "answer": "1",
+    "orgword": "BRAZILIAN"
+});
+practice_problems.push({
+    "probid": "8",
+    "type": "s1",
+    "make": "w1",
+    "age": "12",
+    "weight": "Class 8: 33,001 lb and above (14,969 kg and above)",
+    "region": "Micropolitan",
+    "odometer": "15763",
+    "mileage": "28254",
+    "location": "Left Front",
+    "thickness": "8/32inch",
+    "answer": "3",
+    "orgword": "BRIDAL"
+});
+practice_problems.push({
+    "probid": "9",
+    "type": "s1",
+    "make": "w1",
+    "age": "12",
+    "weight": "Class 8: 33,001 lb and above (14,969 kg and above)",
+    "region": "Medium metro",
+    "odometer": "15763",
+    "mileage": "28254",
+    "location": "Left Front",
+    "thickness": "4/32inch",
+    "answer": "3",
+    "orgword": "SCISSORS"
+});
+practice_problems.push({
+    "probid": "10",
+    "type": "s1",
+    "make": "w1",
+    "age": "12",
+    "weight": "Class 8: 33,001 lb and above (14,969 kg and above)",
+    "region": "Micropolitan",
+    "odometer": "15763",
+    "mileage": "28254",
+    "location": "Left Front",
+    "thickness": "4/32inch",
+    "answer": "1",
+    "orgword": "CINDERELLA"
+});
+practice_problems.push({
+    "probid": "11",
+    "type": "s1",
+    "make": "w1",
+    "age": "12",
+    "weight": "Class 8: 33,001 lb and above (14,969 kg and above)",
+    "region": "Medium metro",
+    "odometer": "15763",
+    "mileage": "28254",
+    "location": "Left Front",
+    "thickness": "6/32inch",
+    "answer": "2",
+    "orgword": "WARRIOR"
+});
